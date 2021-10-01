b2dc0eff90bc0551bfd6b4edf93782a92cdc6530
pj-25 <53967712+pj-25@users.noreply.github.com>
2021-10-01 12:49:27 +0530

    init chitchat

---
+++ Chitchat/app/src/main/java/com/app/chitchat/LauncherActivity.java
@@ -0,0 +1,16 @@
+package com.app.chitchat;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
